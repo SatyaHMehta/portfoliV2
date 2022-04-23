738fa61ddbdb90204f09d4bb29e3fa37ab441b31
Satya Mehta <satya.h.mehta@gmail.com>
2022-04-23 10:55:49 -0500

    edited intro back to what it was after a successful deployment, to see if it still deploys

--- src/componenets/intro/Intro.jsx
+++ src/componenets/intro/Intro.jsx
@@ -1,20 +1,20 @@
 import "./intro.scss";
-// import { init } from "ityped";
-// import { useEffect, useRef } from "react";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 import Satya from '../../assets/SatyaMehta.PNG'
 import DArrow from '../../assets/down.png'
 
 
 export default function Intro() {
-  // const textRef = useRef();
-  // useEffect(() => {
-  //   init(textRef.current, {
-  //     showCursor: true,
-  //     backDelay: 1500,
-  //     backSpeed: 60,
-  //     strings: ["JS", "HTML/CSS", "React", "MERN", "MVC", "OOP"],
-  //   });
-  // }, []);
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["JS", "HTML/CSS", "React", "MERN", "MVC", "OOP"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
     
@@ -32,9 +32,9 @@ export default function Intro() {
           <h3>
             Full-Stack Web Developer
           </h3>
-          {/* <h3>
+          <h3>
             Proficient in: <span ref={textRef}></span>
-          </h3> */}
+          </h3>
         </div>
         <a href="#about">
           <img src={DArrow} alt="" />
